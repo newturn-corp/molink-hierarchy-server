f5af5e8dcfe706503e2a0873139ab32fd80a9a6f
molink-huseong <huseong.lee@molink.life>
2022-01-29 23:14:23 +0900

    문서 생성시 기본값 수정

--- src/Services/ContentService.ts
+++ src/Services/ContentService.ts
@@ -1,11 +1,11 @@
-import { CreateDocumentDTO } from '@newturn-develop/types-molink'
+import { CreateDocumentDTO, DocumentContentInterface } from '@newturn-develop/types-molink'
 import { setAutomergeDocumentAtRedis } from '@newturn-develop/molink-utils'
 import CacheService from './CacheService'
 import Automerge from 'automerge'
 
 class ContentService {
     async createDocument (userId: number, dto: CreateDocumentDTO) {
-        const document = Automerge.from({
+        const document = Automerge.from<DocumentContentInterface>({
             userId,
             children: [{
                 type: 'title',
@@ -15,7 +15,6 @@ class ContentService {
                 category: 'content3',
                 children: [{ text: '' }]
             }],
-            cursor: null,
             lastUsedAt: new Date()
         })
         await setAutomergeDocumentAtRedis(CacheService.contentRedis, dto.id, document)
