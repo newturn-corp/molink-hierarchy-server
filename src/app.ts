4627a2e06c255f0f7398635321f42ee510521f74
molink-huseong <huseong.lee@molink.life>
2022-03-21 05:00:50 +0900

    하이어라키 서버 통계 추가

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,14 @@ import http from 'http'
 import SocketServer from './SocketServer'
 import { useMiddleware } from './Configs/MiddlewareConfig'
 import 'reflect-metadata'
+import { Slack } from '@newturn-develop/molink-utils'
+import env from './env'
+import { startCron } from './cron'
+
+Slack.init(env.slack.token)
 
 const app = express()
 useMiddleware(app)
 const server = http.createServer(app)
 SocketServer.start(server)
+startCron()
