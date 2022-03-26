7db246400a325fd2f1c2c38a36e52f51a1de4dbe
molink-huseong <huseong.lee@molink.life>
2022-03-27 06:05:49 +0900

    cors 추가

--- src/Configs/MiddlewareConfig.ts
+++ src/Configs/MiddlewareConfig.ts
@@ -4,12 +4,23 @@ import morgan from 'morgan'
 import userAgent from 'express-useragent'
 import { useExpressServer } from 'routing-controllers'
 import { routingControllersOptions } from './RoutingConfig'
+import env from '../env'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 export function useMiddleware (app: express.Application) {
+    app.set('port', env.port || 8000)
+    app.use(
+        cors({
+            preflightContinue: true,
+            origin: env.allow_origin_list.split(',')
+        })
+    )
     morgan.token('date', () => {
         return moment().format('YYYY-MM-DD HH:mm:ss')
     })
-    const logFormat = ':remote-addr [:date[clf]] ":method :url" :status :res[content-length] - :response-time ms ":user-agent"'
+    const logFormat = ':req[X-Real-IP] [:date[clf]] ":method :url" :status :res[content-length] - :response-time ms ":user-agent"'
+    app.use(cookieParser(env.secret.cookie))
     app.use(morgan(logFormat))
     app.use(userAgent.express())
     useExpressServer(app, routingControllersOptions)
